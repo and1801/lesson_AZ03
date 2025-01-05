577dcec7c751fc46fa8bf7b4f368a1183a4e9b4f
and1801 <72920393+and1801@users.noreply.github.com>
2025-01-06 00:44:32 +0800

    hometask3

--- ht3-aver.py
+++ ht3-aver.py
@@ -1,14 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-
-
+# Путь к файлу CSV
 file_path = 'pricesd.csv'
+
+# Чтение данных из CSV-файла
 data = pd.read_csv(file_path)
 
+# Удаление пробелов из строковых представлений чисел
+data['Price'] = data['Price'].str.replace(' ', '')
+
+# Преобразование столбца 'Price' в числовой формат
 data['Price'] = pd.to_numeric(data['Price'], errors='coerce')
 
+# Удаление NaN значений, если они есть
 data = data.dropna(subset=['Price'])
 
-ap = data['Price'].mean()
+# Вычисление среднего значения
+average_price = data['Price'].mean()
 
-print(ap)
+# Вывод среднего значения
+print(f'Средняя цена: {average_price}')
